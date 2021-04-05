f0c3e1f52a6010c7bb06a3194d22ee9971888e20
Michal 'vorner' Vaner <vorner@vorner.cz>
2021-04-05 17:52:58 +0200

    Metrics for spans
    * All the ones on events
    * Timers and levels too

--- examples/shaving.rs
+++ examples/shaving.rs
@@ -2,40 +2,49 @@ use std::thread;
 use std::time::Duration;
 
 use dipstick::{AtomicBucket, ScheduleFlush, Stream};
+use log::LevelFilter;
 use tracing::{debug, info_span, subscriber};
 use tracing_dipstick::DipstickLayer;
-use tracing_subscriber::fmt::Layer as FmtLayer;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::Registry;
 
 fn main() {
+    /*
+     * We use the log-always integration of tracing here and route that to the env logger, that has
+     * INFO enabled by default and can override by RUST_LOG to something else.
+     *
+     * We could use tracing_subscriber::fmt, *but* the EnvFilter there unfortunately disables
+     * events/spans for the whole stack, not for logging only. And we want all the metrics while we
+     * want only certain level of events.
+     */
+    env_logger::builder()
+        .filter_level(LevelFilter::Info)
+        .parse_default_env()
+        .init();
+
     let root = AtomicBucket::new();
     root.stats(dipstick::stats_all);
     root.drain(Stream::write_to_stdout());
     let _flush = root.flush_every(Duration::from_secs(5));
 
     let bridge = DipstickLayer::new(root);
-
-    let subscriber = Registry::default()
-        // FIXME: This, unfortunately, filters the metrics too, no matter if the metrics are inside
-        // or outside of it :-(
-        //.with(EnvFilter::from_default_env())
-        .with(FmtLayer::new().pretty())
-        .with(bridge);
+    let subscriber = Registry::default().with(bridge);
 
     subscriber::set_global_default(subscriber).unwrap();
 
     const CNT: usize = 100;
-    let _yaks = info_span!("Shaving yaks", cnt = CNT).entered();
+    let _yaks = info_span!("Shaving yaks", cnt = CNT, metrics.scope = "shaving").entered();
     for i in 0..100 {
-        let this_yak = info_span!("Yak", no = i, metric_scope = "yak");
-        this_yak.in_scope(|| {
-            debug!("Starting shaving");
-        });
-        debug!("Waiting for yak to calm down");
+        let _this_yak = info_span!(
+            "Yak",
+            no = i,
+            metrics.scope = "yak",
+            metrics.time = "time",
+            metrics.level = "active"
+        )
+        .entered();
+        debug!(metrics.counter = "started", "Starting shaving");
         thread::sleep(Duration::from_millis(600));
-        this_yak.in_scope(|| {
-            debug!(metric_counter = "done", "Shaving done");
-        });
+        debug!(metrics.counter = "done", "Shaving done");
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,67 @@
 use std::fmt::Debug;
 
-use dipstick::{InputScope, Prefixed};
-use tracing_core::{Event, Subscriber};
+use dipstick::{InputScope, Level, Prefixed, TimeHandle, Timer};
 use tracing_core::field::{Field, Visit};
 use tracing_core::span::{Attributes, Id};
+use tracing_core::{Event, Subscriber};
 use tracing_subscriber::layer::{Context, Layer};
 use tracing_subscriber::registry::LookupSpan;
 
-const SCOPE_NAME: &str = "metric_scope";
-const SCOPE_NAME_FULL: &str = "metric_scope_full";
+const SCOPE_NAME: &str = "metrics.scope";
+const SCOPE_NAME_FULL: &str = "metrics.scope.full";
 
-const VALUE: &str = "metric_value";
-const COUNTER: &str = "metric_counter";
-const GAUGE: &str = "metric_gauge";
+const VALUE: &str = "metrics.value";
+const COUNTER: &str = "metrics.counter";
+const GAUGE: &str = "metrics.gauge";
+
+const TIME: &str = "metrics.time";
+const LEVEL: &str = "metrics.level";
 
 #[derive(Clone)]
-struct Scope<S>(S);
+struct Scope<S> {
+    scope: S,
+    timer: Option<(Timer, TimeHandle)>,
+    // TODO: CPU timers
+    level: Option<Level>,
+    value: i64,
+}
+
+impl<S: InputScope> Visit for Scope<S> {
+    fn record_debug(&mut self, _: &Field, _: &dyn Debug) {}
+    fn record_str(&mut self, field: &Field, value: &str) {
+        let name = field.name();
+        if name == TIME {
+            let timer = self.scope.timer(value);
+            let start = timer.start();
+            self.timer = Some((timer, start));
+        } else if name == LEVEL {
+            let level = self.scope.level(value);
+            level.adjust(self.value);
+            self.level = Some(level);
+        } else if name == COUNTER {
+            self.scope.counter(value).count(self.value as _);
+        } else if name == GAUGE {
+            self.scope.gauge(value).value(self.value);
+        }
+    }
+}
+
+struct ValueVisitor<'a>(&'a mut i64);
+
+impl Visit for ValueVisitor<'_> {
+    fn record_debug(&mut self, _: &Field, _: &dyn Debug) {}
+    fn record_i64(&mut self, field: &Field, value: i64) {
+        if field.name() == VALUE {
+            *self.0 = value;
+        }
+    }
+    fn record_u64(&mut self, field: &Field, value: u64) {
+        if field.name() == VALUE {
+            // TODO: Is this OK?
+            *self.0 = value as _;
+        }
+    }
+}
 
 #[derive(Copy, Clone, Debug, Default)]
 pub struct DipstickLayer<S> {
@@ -27,9 +73,7 @@ where
     S: Clone + InputScope + Prefixed + 'static,
 {
     pub fn new(input_scope: S) -> Self {
-        DipstickLayer {
-            scope: input_scope,
-        }
+        DipstickLayer { scope: input_scope }
     }
 }
 
@@ -73,13 +117,22 @@ where
                 current
                     .extensions()
                     .get::<Scope<S>>()
-                    .map(|Scope(s)| named(s))
+                    .map(|Scope { scope: s, .. }| named(s))
             })
             .unwrap_or_else(|| named(&self.scope));
+        let mut scope = Scope {
+            scope,
+            timer: None,
+            level: None,
+            value: 1,
+        };
+        attrs.record(&mut ValueVisitor(&mut scope.value));
+        attrs.record(&mut scope);
+
         ctx.span(id)
             .expect("Missing newly created span")
             .extensions_mut()
-            .insert(Scope(scope));
+            .insert(scope);
     }
     // TODO: How about cloning/creating new IDs for spans?
     fn on_event(&self, event: &Event, ctx: Context<I>) {
@@ -88,26 +141,16 @@ where
             .lookup_current()
             .map(|current| {
                 // FIXME: The clone!
-                current.extensions().get::<Scope<S>>().cloned().expect("Missing prepared scope").0
+                current
+                    .extensions()
+                    .get::<Scope<S>>()
+                    .cloned()
+                    .expect("Missing prepared scope")
+                    .scope
             })
             .unwrap_or_else(|| self.scope.clone());
 
         let mut value = 1i64;
-        struct ValueVisitor<'a>(&'a mut i64);
-        impl Visit for ValueVisitor<'_> {
-            fn record_debug(&mut self, _: &Field, _: &dyn Debug) {}
-            fn record_i64(&mut self, field: &Field, value: i64) {
-                if field.name() == VALUE {
-                    *self.0 = value;
-                }
-            }
-            fn record_u64(&mut self, field: &Field, value: u64) {
-                if field.name() == VALUE {
-                    // TODO: Is this OK?
-                    *self.0 = value as _;
-                }
-            }
-        }
         event.record(&mut ValueVisitor(&mut value));
 
         struct MetricVisitor<'a, S> {
@@ -130,5 +173,18 @@ where
             value,
         });
     }
-    // TODO: on_span exit/enter/...
+
+    fn on_close(&self, id: Id, ctx: Context<'_, I>) {
+        let current = ctx.span(&id).expect("Missing dying span");
+        let exts = current.extensions();
+        let scope: &Scope<S> = exts.get().expect("Missing span scope");
+
+        if let Some((timer, start)) = scope.timer.as_ref() {
+            timer.stop(*start);
+        }
+
+        if let Some(level) = scope.level.as_ref() {
+            level.adjust(-scope.value);
+        }
+    }
 }
